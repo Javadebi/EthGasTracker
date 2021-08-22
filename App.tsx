a4052893262d86938676dc111d8ce95350fc30ec
Mj <mj.ebrahimzadeh@gmail.com>
2021-08-22 21:20:34 +0430

    stable version

---
+++ App.tsx
@@ -0,0 +1,300 @@
+import React, {useEffect, useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Pressable,
+  StatusBar,
+  ToastAndroid,
+} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import PushNotification from 'react-native-push-notification';
+import BackgroundService from 'react-native-background-actions';
+import ModalPicker from './ModalPicker';
+
+PushNotification.createChannel(
+  {
+    channelId: '1',
+    channelName: 'gasprice',
+  },
+  created => {
+    console.log(created);
+  },
+);
+
+export const getData = async (item: string) => {
+  try {
+    const value = await AsyncStorage.getItem(item);
+    if (value !== null) {
+      return value;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const storeData = async (name: string, item: string) => {
+  try {
+    await AsyncStorage.setItem(name, item);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const options = {
+  taskName: 'Gas Track',
+  taskTitle: 'Tracking the gas :)',
+  taskDesc: 'Checking gas price when idle',
+  taskIcon: {
+    name: 'ic_launcher',
+    type: 'mipmap',
+  },
+  color: '#6C747C',
+};
+
+// const showAlert = () => {
+//   Alert.alert('Error', 'Something Went Wrong', [
+//     {
+//       text: 'Exit',
+//       onPress: () => {
+//         console.log('hello');
+//       },
+//     },
+//   ]);
+// };
+
+const toastWarning = () => {
+  ToastAndroid.showWithGravity(
+    'Network connection failed!',
+    ToastAndroid.SHORT,
+    ToastAndroid.BOTTOM,
+  );
+};
+
+const getGasValues = async () => {
+  try {
+    const response = await fetch(
+      'https://ethgasstation.info/api/ethgasAPI.json?api-key=025b0876eb0a85ae64db5842f45cbacbec4a496f8dc46d5d10096fc8ab03',
+    );
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default function App() {
+  const backgroundJobStart = async () => {
+    await BackgroundService.start(backgroundJob, options)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  const backgroundJob = async () => {
+    await new Promise(async () => {
+      setInterval(() => {
+        getGasValues().then(async result => {
+          const selected = await getData('selected')
+            .then(res => {
+              return res;
+            })
+            .catch(err => {
+              return err;
+            });
+          const push_notifications = await getData('push_notifications')
+            .then(res => {
+              return res;
+            })
+            .catch(err => {
+              return err;
+            });
+          const limit = await getData('limit')
+            .then(res => {
+              return res;
+            })
+            .catch(err => {
+              return err;
+            });
+          if (
+            push_notifications === 'true' &&
+            typeof selected !== 'undefined' &&
+            typeof push_notifications !== 'undefined' &&
+            typeof limit !== 'undefined'
+          ) {
+            if (
+              parseInt(limit, 10) !== 0 &&
+              result[typeof selected !== 'undefined' ? selected : 'safeLow'] /
+                10 <
+                limit
+            ) {
+              PushNotification.localNotification({
+                message: `Gas is lower than ${limit} gwei`,
+                channelId: '1',
+              });
+              await storeData('push_notifications', 'false');
+              await storeData('isSet', 'false');
+            }
+          }
+        });
+      }, 1000);
+    });
+  };
+  const [gas, setGas] = useState({
+    fastGas: {gwei: 0, time: 0},
+    instantGas: {gwei: 0, time: 0},
+    normalGas: {gwei: 0, time: 0},
+    slowGas: {gwei: 0, time: 0},
+  });
+  const [modalVisible, setModalVisible] = useState(false);
+
+  useEffect(() => {
+    if (!BackgroundService.isRunning()) {
+      backgroundJobStart();
+    }
+    const interval = setInterval(() => {
+      getGasValues()
+        .then(result => {
+          setGas({
+            fastGas: {
+              gwei: result.fast / 10,
+              time: result.fastWait,
+            },
+            instantGas: {
+              gwei: result.fastest / 10,
+              time: result.fastestWait,
+            },
+            normalGas: {
+              gwei: result.average / 10,
+              time: result.avgWait,
+            },
+            slowGas: {
+              gwei: result.safeLow / 10,
+              time: result.safeLowWait,
+            },
+          });
+        })
+        .catch(err => {
+          console.log(err);
+          toastWarning();
+        });
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <View style={styles.mainContainer}>
+      <StatusBar animated={true} backgroundColor="#202224" />
+      <ModalPicker
+        isSet={true}
+        modalVisible={modalVisible}
+        setModalVisible={() => setModalVisible(!modalVisible)}
+        setIsSet={async () => {
+          await storeData('isSet', 'false');
+        }}
+      />
+      <ScrollView
+        style={styles.scrollViewStyle}
+        contentContainerStyle={styles.container}>
+        {/* <Pressable
+          style={{
+            backgroundColor: 'red',
+            position: 'absolute',
+            left: 0,
+            top: 10,
+            zIndex: 1,
+            width: 50,
+          }}
+          onPress={() => {
+            console.log('hell');
+          }}>
+          <Text>Hello</Text>
+        </Pressable> */}
+        <View style={styles.gasContainer}>
+          <Text style={styles.title}>SLOW</Text>
+          <Text style={styles.gwei}>{gas.slowGas.gwei} gwei</Text>
+          <Text style={styles.time}>{`< ${gas.slowGas.time} minutes`}</Text>
+        </View>
+        <View style={styles.gasContainer}>
+          <Text style={styles.title}>NORMAL</Text>
+          <Text style={styles.gwei}>{gas.normalGas.gwei} gwei</Text>
+          <Text style={styles.time}>{`< ${gas.normalGas.time} minutes`}</Text>
+        </View>
+        <View style={styles.gasContainer}>
+          <Text style={styles.title}>FAST</Text>
+          <Text style={styles.gwei}>{gas.fastGas.gwei} gwei</Text>
+          <Text style={styles.time}>{`< ${gas.fastGas.time} minutes`}</Text>
+        </View>
+        <View style={styles.gasContainer}>
+          <Text style={styles.title}>INSTANT</Text>
+          <Text style={styles.gwei}>{gas.instantGas.gwei} gwei</Text>
+          <Text style={styles.time}>{`< ${gas.instantGas.time} minutes`}</Text>
+        </View>
+        <Pressable
+          onPress={() => {
+            setModalVisible(!modalVisible);
+          }}>
+          <Text style={styles.button}>SET NOTIFICATION</Text>
+        </Pressable>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#222629',
+    alignItems: 'center',
+    padding: 20,
+  },
+  gasContainer: {
+    backgroundColor: '#383E4D',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderWidth: 1,
+    padding: 20,
+    minWidth: 170,
+    minHeight: 170,
+    borderRadius: 20,
+    marginBottom: 40,
+  },
+  title: {
+    color: '#6C747C',
+    fontWeight: 'bold',
+    fontSize: 17,
+  },
+  gwei: {
+    color: '#D4D4DC',
+    fontWeight: '700',
+    fontSize: 15,
+  },
+  price: {
+    color: '#D4D4DC',
+  },
+  time: {
+    color: '#6C747C',
+    fontSize: 13,
+  },
+  button: {
+    borderWidth: 1,
+    padding: 17,
+    backgroundColor: '#4e75b5',
+    borderRadius: 10,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  scrollViewStyle: {
+    flex: 1,
+    backgroundColor: '#222629',
+  },
+  mainContainer: {
+    flex: 1,
+  },
+});
---
+++ ModalPicker.tsx
@@ -0,0 +1,170 @@
+import React, {FC, useState} from 'react';
+import {
+  View,
+  Text,
+  Pressable,
+  Modal,
+  StyleSheet,
+  TextInput,
+} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import {storeData} from './App';
+
+const ModalPicker: FC<ModalPickerProps> = ({
+  isSet,
+  modalVisible,
+  setIsSet,
+  setModalVisible,
+}) => {
+  const [selectedValue, setSelectedValue] = useState('safeLow');
+  const [smallerNumber, setSmallerNumber] = useState('');
+  // const [biggerNumber, setBiggerNumber] = useState("");
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => setModalVisible()}>
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <Text style={{color: '#fff', fontWeight: 'bold', fontSize: 17}}>
+            Select Gas Speed
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+            <View style={styles.pickerView}>
+              <Picker
+                selectedValue={selectedValue}
+                onValueChange={(itemValue: string) => {
+                  setSelectedValue(itemValue);
+                }}
+                style={styles.picker}
+                mode="dropdown">
+                <Picker.Item label="Slow" value="safeLow" />
+                <Picker.Item label="Normal" value="average" />
+                <Picker.Item label="Fast" value="fast" />
+                <Picker.Item label="Instant" value="fastest" />
+              </Picker>
+            </View>
+            <Text style={styles.buttonText}>{'<'}</Text>
+            <TextInput
+              style={styles.textInput}
+              keyboardType="numeric"
+              value={smallerNumber}
+              onChangeText={value => {
+                setSmallerNumber(value);
+              }}
+              placeholder="0"
+              placeholderTextColor="#fff"
+            />
+          </View>
+          <View style={styles.buttonsView}>
+            <Pressable style={styles.button} onPress={() => setModalVisible()}>
+              <Text style={styles.buttonText}>Cancel</Text>
+            </Pressable>
+            <Pressable
+              style={styles.button}
+              onPress={async () => {
+                setModalVisible();
+                await storeData('push_notifications', 'true');
+                await storeData('selected', selectedValue);
+                await storeData('limit', `${smallerNumber}`);
+                setIsSet();
+              }}>
+              <Text style={styles.buttonText}>Set</Text>
+            </Pressable>
+          </View>
+          {isSet ? (
+            <Pressable
+              style={styles.clearButton}
+              onPress={async () => {
+                setSmallerNumber('');
+                await storeData('isSet', 'false');
+                await storeData('push_notifications', 'false');
+                setModalVisible();
+              }}>
+              <Text style={styles.buttonText}>Clear</Text>
+            </Pressable>
+          ) : null}
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+export default ModalPicker;
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: '#383E4D',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  picker: {
+    height: 40,
+    width: 120,
+    color: '#fff',
+  },
+  button: {
+    borderRadius: 10,
+    padding: 15,
+    elevation: 2,
+    backgroundColor: '#4e75b5',
+    margin: 10,
+  },
+  buttonsView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+  },
+  textInput: {
+    height: 45,
+    margin: 10,
+    width: 50,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+    borderColor: '#fff',
+    color: '#fff',
+  },
+  pickerView: {
+    borderWidth: 1,
+    borderRadius: 20,
+    margin: 10,
+    borderColor: '#fff',
+  },
+  clearButton: {
+    borderRadius: 10,
+    padding: 15,
+    elevation: 2,
+    backgroundColor: '#bd2424',
+    margin: 10,
+  },
+});
+
+interface ModalPickerProps {
+  isSet: boolean;
+  modalVisible: boolean;
+  setIsSet: () => void;
+  setModalVisible: () => void;
+}
